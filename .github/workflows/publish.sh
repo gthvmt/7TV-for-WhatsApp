365c6cc7d663531f787e65845f7aab28edb707b7
gthvmt <gthvmt@protonmail.com>
2023-12-19 00:12:34 +0100

    💚 fix publish script
     use the new preferred method to install the latest node version

--- .github/workflows/publish.sh
+++ .github/workflows/publish.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-curl -fsSL https://deb.nodesource.com/setup_20.x | bash - &&\
+
+# install node
+apt install -y ca-certificates curl gnupg
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+apt update
 apt install -y nodejs
+
+# install & run semantic-release
 npm install -g semantic-release @semantic-release/git semantic-release-gitmoji semantic-release-flutter-plugin
 npx semantic-release
